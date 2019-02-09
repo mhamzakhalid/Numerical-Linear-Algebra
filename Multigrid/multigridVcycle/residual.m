05dce389adf9631ef4faeb532549dc57a9f24043
Muhammad Hamza Khalid <37152213+mhamzakhalid@users.noreply.github.com>
2019-02-10 00:10:13 +0100

    Final Changes

--- Multigrid/multigridVcycle/residual.m
+++
@@ -1,8 +0,0 @@
-function rh =residual(U,rhs)
-    n = length(rhs);
-    ind = 2:n-1;
-    rh = zeros(n,n);
-    Au = matvec(U,rhs);
-    rh(ind,ind) = rhs(ind,ind) -  Au(ind,ind);
-end                      
-                        
